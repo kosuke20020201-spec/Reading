41279ab22087b0b9dcf96628220c8eb2c3944688
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-23 09:28:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260622-shelf-drag-tag-branch-1';
+const CACHE_NAME = 'reading-note-pwa-20260622-clean-shelf-target-reorder-1';
 const APP_SHELL = [
   './',
   './index.html',
