1261f32e38c490aeee7faf88d2adfa836bbac2b2
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-24 10:14:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260624-tag-tray-scroll-top-1';
+const CACHE_NAME = 'reading-note-pwa-20260624-sticky-tag-tray-1';
 const APP_SHELL = [
   './',
   './index.html',
