99558055f761aa4ebb7c236df0bfa289b52e30d6
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-28 12:28:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260628-author-dedupe-v73';
+const CACHE_NAME = 'reading-note-pwa-20260628-original-choice-spine-v74';
 const APP_SHELL = [
   './',
   './index.html',
