016d53e338973f3d27531d17046559c6fe505e07
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-28 22:02:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260628-tag-folder-fast-cover-v81';
+const CACHE_NAME = 'reading-note-pwa-20260628-openbd-cover-v82';
 const APP_SHELL = [
   './',
   './index.html',
