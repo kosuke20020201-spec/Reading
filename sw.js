0ea7638ec61ef2ee784bda20088b0f28e5f644b6
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-28 11:13:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260628-book-info-api-1';
+const CACHE_NAME = 'reading-note-pwa-20260628-same-page-created-order-1';
 const APP_SHELL = [
   './',
   './index.html',
