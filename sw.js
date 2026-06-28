04bfc425a246fbf0930240f993fe85c19eb8ed10
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-28 19:00:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260628-book-info-label-v77';
+const CACHE_NAME = 'reading-note-pwa-20260628-cover-fallback-v78';
 const APP_SHELL = [
   './',
   './index.html',
