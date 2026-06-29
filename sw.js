3da210fa5ef583e0911e1fb922baed42ac4da993
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-29 11:37:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260629-force-candidate-choice-v91';
+const CACHE_NAME = 'reading-note-pwa-20260629-book-wide-lanes-v92';
 const APP_SHELL = [
   './',
   './index.html',
