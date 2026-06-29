ce3ac737204b4a941c8023beceb8c659eb6a6cd2
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-29 09:59:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260629-bookshelf-cover-v87';
+const CACHE_NAME = 'reading-note-pwa-20260629-refresh-button-v89';
 const APP_SHELL = [
   './',
   './index.html',
