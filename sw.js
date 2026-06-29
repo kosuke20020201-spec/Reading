bb2500a994f4598cbe9d1c2d6b59d5eed1f19e4e
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-06-29 10:11:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260629-refresh-button-v89';
+const CACHE_NAME = 'reading-note-pwa-20260629-inline-handler-fix-v90';
 const APP_SHELL = [
   './',
   './index.html',
