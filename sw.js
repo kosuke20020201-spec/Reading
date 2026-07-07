52ee2a78a41f1c6e84686973870bdb753bfb8c4e
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-07-07 09:39:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260706-tag-auto-folder-v129';
+const CACHE_NAME = 'reading-note-pwa-20260707-tag-auto-folder-v131';
 const APP_SHELL = [
   './',
   './index.html',
