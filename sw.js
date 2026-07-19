1f3764d91f099a0192ab9ff477d83da57d727817
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-07-19 13:27:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260719-ndl-unrestricted-query-first-v168';
+const CACHE_NAME = 'reading-note-pwa-20260719-nested-chapter-summary-v169';
 const APP_SHELL = [
   './',
   './index.html',
