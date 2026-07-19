9604b9fd1a3b4a1c2ab9f8752305d8648edb3a25
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-07-19 21:43:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260719-cover-check-timeout-dedup-v171';
+const CACHE_NAME = 'reading-note-pwa-20260719-lazy-cover-check-v172';
 const APP_SHELL = [
   './',
   './index.html',
