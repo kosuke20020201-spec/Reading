181ab17d8f515319922cc643beddacac018df6e7
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-07-26 21:42:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260726-fix-card-scroll-jump-v181';
+const CACHE_NAME = 'reading-note-pwa-20260726-remove-card-content-visibility-v182';
 const APP_SHELL = [
   './',
   './index.html',
