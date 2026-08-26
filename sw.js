e6b360050f26b8582c9fb4a08b3741bb02c62b91
kosuke20020201-spec <kosuke20020201@gmail.com>
2026-08-26 09:06:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reading-note-pwa-20260826-badge-reserved-space-v199';
+const CACHE_NAME = 'reading-note-pwa-20260826-entry-bookmark-v200';
 const APP_SHELL = [
   './',
   './index.html',
